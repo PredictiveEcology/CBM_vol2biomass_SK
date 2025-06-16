5789e90eccd350dbbdd5c9a9697e818a4aaf327e
camillegiuliano <camille.giuliano@nrcan-rncan.gc.ca>
2025-06-16 10:33:36 -0700

    change name to CBM_vol2biomass_SK

--- CBM_vol2biomass_SK.R
+++ CBM_vol2biomass_SK.R
@@ -1,5 +1,5 @@
 defineModule(sim, list(
-  name = "CBM_vol2biomass",
+  name = "CBM_vol2biomass_SK",
   description = paste("A module to prepare the user-provided growth and yield information for use",
                       "in the family of models spadesCBM - CBM-CFS3-like simulation of forest",
                       "carbon in the platform SpaDES. This module takes in user-provided m3/ha",
@@ -10,11 +10,11 @@ defineModule(sim, list(
     person("Céline", "Boisvenue", email = "celine.boisvenue@nrcan-rncan.gc.ca", role = c("aut", "cre"))
   ),
   childModules = character(0),
-  version = list(CBM_vol2biomass = "0.0.0.9000"),
+  version = list(CBM_vol2biomass_SK = "0.0.0.9000"),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
   citation = list("citation.bib"),
-  documentation = deparse(list("README.txt", "CBM_vol2biomass.Rmd")),
+  documentation = deparse(list("README.txt", "CBM_vol2biomass_SK.Rmd")),
   reqdPkgs = list(
     "PredictiveEcology/CBMutils@development", "PredictiveEcology/LandR@development",
     "ggforce", "ggplot2", "ggpubr", "googledrive", "mgcv", "quickPlot", "robustbase", "data.table"
@@ -154,7 +154,7 @@ defineModule(sim, list(
 ## event types
 #   - type `init` is required for initialization
 
-doEvent.CBM_vol2biomass <- function(sim, eventTime, eventType) {
+doEvent.CBM_vol2biomass_SK <- function(sim, eventTime, eventType) {
   switch(
     eventType,
     init = {
@@ -165,8 +165,8 @@ doEvent.CBM_vol2biomass <- function(sim, eventTime, eventType) {
       sim <- Init(sim)
 
       # schedule future event(s)
-      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "CBM_vol2biomass", "plot")
-      sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "CBM_vol2biomass", "save")
+      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "CBM_vol2biomass_SK", "plot")
+      sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "CBM_vol2biomass_SK", "save")
     },
     plot = {
       # ! ----- EDIT BELOW ----- ! #
